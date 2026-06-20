91f51ec83c73171d4218220fc4259a3efdbba011
Parisha <47680537+parishamaheshj18@users.noreply.github.com>
2026-06-20 01:27:36 -0400

    Add: eating-bananas - submission-6

---
+++ Data Structures & Algorithms/eating-bananas/submission-6.py
@@ -0,0 +1,52 @@
+import math
+
+class Solution:
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        # piles[i] - number of bananas in ith pile, len(piles) - how many piles
+        # h - hours given to finish all the bananas
+        # k - I have to decide bananas/hour - (sum(piles)/h)
+
+        # [1] - 1
+        # [4] - 2
+        # [3] - 2
+        # [2] - 1
+        low = 1
+        high = max(piles)
+        res = high
+        if h == len(piles):
+            return high
+        while low<=high:
+            k_mid = (low+high)//2
+            if self.is_k_ok(piles,k_mid,h) > 0:
+                low = k_mid + 1
+                
+            elif self.is_k_ok(piles,k_mid,h) < 0:
+                res = k_mid
+                high = k_mid-1
+            elif self.is_k_ok(piles,k_mid,h) == 0:
+                res = k_mid
+                high = k_mid -1
+        return res
+        
+
+    def is_k_ok(self,piles,k,h):
+        hours = 0
+        for num in piles:
+            if num/k == num//k:
+                hours += num/k
+            else:
+                hours += num//k +1
+
+        if hours > h:
+            return 1
+        elif hours < h:
+            return -1
+        elif hours==h:
+            return 0
+
+        
+        
+        
+
+        
+
